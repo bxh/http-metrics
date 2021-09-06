7d0c89d83eb9324fa7ffb4574b7e9603c02f60ef
Bangxin Hu <hu.bangxin@gmail.com>
2021-09-06 15:23:03 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const AWS = require('aws-sdk')
+const axios = require('axios')
+
+// Name of a service, any string
+const serviceName = process.env.SERVICE_NAME
+// URL of a service to test
+const url = process.env.URL
+
+// CloudWatch client
+const cloudwatch = new AWS.CloudWatch();
+
+exports.handler = async (event) => {
+  let endTime
+  let requestWasSuccessful
+
+  const startTime = timeInMs()
+  try {
+    await axios.get(url)
+    requestWasSuccessful = true
+  } catch(e) {
+    requestWasSuccessful = false
+  } finally {
+    endTime = timeInMs()
+  }
+
+  const lapse = endTime - startTime
+  await cloudwatch.putMetricData({
+    MetricData: [
+      {
+        MetricName: 'Success',
+        Dimensions: [
+          {
+            Name: 'ServiceName',
+            Value: serviceName
+          }
+        ],
+        Unit: 'Count',
+        Value: requestWasSuccessful ? 1 : 0
+      }
+    ],
+    Namespace: 'Udacity/Serveless'
+  }).promise()
+
+  await cloudwatch.putMetricData({
+    MetricData: [
+      {
+        MetricName: 'Latency',
+        Dimensions: [
+          {
+            Name: 'ServiceName',
+            Value: serviceName
+          }
+        ],
+        Unit: 'Milliseconds',
+        Value: lapse 
+      }
+    ],
+    Namespace: 'Udacity/Serveless'
+  }).promise()
+}
+
+function timeInMs() {
+  return new Date().getTime()
+}
